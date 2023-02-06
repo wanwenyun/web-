b1159d884f6f51df454c712fafd1c75297bdd12e
wanwenyun <wywan@stu.xmu.edu.cn>
2023-02-06 22:34:32 +0800

    补充index.md

---
+++ 算法/leetcode/283.移动零.js
@@ -0,0 +1,18 @@
+/*
+ * @lc app=leetcode.cn id=283 lang=javascript
+ *
+ * [283] 移动零
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ * 1. 滑动窗口
+ * 2. 指针法
+ */
+var moveZeroes = function(nums) {
+
+};
+// @lc code=end
+
