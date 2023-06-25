f6273a6fa3136857dc8c0473cbff1ee29148bad9
wenyunwan <wywan@stu.xmu.edu.cn>
2023-06-25 17:03:40 +0800

    大数相加

--- 手写代码/手撕代码/大整数运算.js
+++ 手写代码/手撕代码/大整数运算.js
@@ -7,13 +7,13 @@ function bigNumAdd(a,b) {
 	var a = a.split('');
 	var b = b.split('');
 	var res = [];
-	var carry = 0;
+	var carry = 0; // 记录进位
 	var len = Math.max(a.length,b.length);
 	var i = len;
 	while(i--){
 		var sum = (+a[i - len + a.length] || 0) + (+b[i - len + b.length] || 0) + carry;
 		carry = parseInt(sum / 10); // parseInt转1十进制，计算进位
-		res.unshift(sum % 10);
+		res.unshift(sum % 10); // 余数， 将元素添加到数组的开头
 	}
 	if (carry) res.unshift(carry);
     return res.join(''); // 数组转字符串
